71b0287d901414444b93632944e0cb956d44cb65
Yurii <paraska108@gmail.com>
2022-10-18 13:30:16 +0300

    redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {legacy_createStore as createStore} from 'redux';
 
-console.log('Hello Redux');
+// const initialState = 0;
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+
+store.dispatch({ type: 'INC' });
+store.dispatch({ type: 'INC' });
+
+
+// let state = reducer(initialState, { type: 'INC' });
+// state = reducer(state, { type: 'INC' });
+// state = reducer(state, { type: 'INC' });
+
+
+// console.log(state);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <>
+
     </>
   </React.StrictMode>,
 );
